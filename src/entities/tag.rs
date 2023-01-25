91776d220d2f5c06df679f68a8c179ada6def6f8
Benedikt Terhechte <terhechte@gmail.com>
2023-01-25 18:14:03 +0100

    make `following` optional

--- src/entities/tag.rs
+++ src/entities/tag.rs
@@ -7,5 +7,6 @@ pub struct Tag {
     pub name: String,
     pub url: String,
     pub history: Option<Vec<History>>,
+    #[serde(default)]
     pub following: bool,
 }
--- src/mastodon/entities/tag.rs
+++ src/mastodon/entities/tag.rs
@@ -7,6 +7,7 @@ pub struct Tag {
     name: String,
     url: String,
     history: Option<Vec<History>>,
+    #[serde(default)]
     following: bool,
 }
 
