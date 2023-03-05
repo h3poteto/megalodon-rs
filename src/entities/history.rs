52d41ea940b97361919d9acc7fa339d2fdb3bef8
networkException <git@nwex.de>
2023-03-05 20:42:51 +0100

    Add parse_from_string for deserializing history entities
    This patch adds a new function to parse string values to u64, usize and
    possibly other types and uses it to deserialize field values in the
    history mastodon and pleroma entities.

--- src/entities/history.rs
+++ src/entities/history.rs
@@ -6,3 +6,14 @@ pub struct History {
     pub uses: usize,
     pub accounts: usize,
 }
+
+pub fn parse_from_string<'de, T, D>(deserializer: D) -> Result<T, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+        T: std::str::FromStr,
+        <T as std::str::FromStr>::Err: std::fmt::Display,
+{
+    Ok(String::deserialize(deserializer)?
+        .parse()
+        .map_err(serde::de::Error::custom)?)
+}
